bd4c75b6a294aeb49bffb45178ea9b6a5c4345eb
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-11-22 17:41:05 +0800

    testing added to test/system/questions_test

---
+++ test/system/questions_test.rb
@@ -0,0 +1,29 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "saying right answer to try response from the coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work right now!"
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+
+  test "testing answer contains p text you" do
+    visit answer_url
+    assert_selector "p", text: "You"
+  end
+end
